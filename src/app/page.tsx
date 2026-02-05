ca8d8c2be35a25fe4989aa797de212aac1ba98eb
George Arney <georgearney@outlook.com>
2026-02-05 21:57:37 +0000

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="flex flex-col items-center justify-center gap-4 text-center">
+        <h1 className="text-6xl font-bold tracking-tight">
+          Hackathon ETC Oxford 2026
+        </h1>
+      </main>
+    </div>
+  );
+}
