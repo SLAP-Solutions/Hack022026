bfd4151784e2baa71d53d6dd4213eca7dee3ab1b
George Arney <georgearney@outlook.com>
2026-02-06 22:02:00 +0000

    Additional page

---
+++ src/app/insurance/page.tsx
@@ -0,0 +1,9 @@
+export default function insurance() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="flex flex-col items-center justify-center gap-4 text-center">
+        <h1 className="text-6xl font-bold tracking-tight">Insurance</h1>
+      </main>
+    </div>
+  );
+}
