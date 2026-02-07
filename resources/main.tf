88411e5fc834fe9726dec13eb02e85777e9fba6b
Ben Summers <bensummersonline@gmail.com>
2026-02-07 01:16:59 +0000

    change

--- resources/main.tf
+++ resources/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_resource_group" "main" {
   }
 }
 
-resource "azurerm_service_plan" "nextjs" {
+resource "azurerm_service_plan" "app_service_plan" {
   name                = azurecaf_name.app_service_plan.result
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
@@ -43,11 +43,11 @@ resource "azurerm_service_plan" "nextjs" {
   }
 }
 
-resource "azurerm_linux_web_app" "nextjs" {
+resource "azurerm_linux_web_app" "webapp" {
   name                = azurecaf_name.webapp.result
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  service_plan_id     = azurerm_service_plan.nextjs.id
+  service_plan_id     = azurerm_service_plan.app_service_plan.id
 
   site_config {
     application_stack {
--- resources/variables.tf
+++ resources/variables.tf
@@ -1,5 +1,5 @@
 variable "location" {
   description = "Azure location for resources"
   type        = string
-  default     = "UK South"
+  default     = "westeurope"
 }
